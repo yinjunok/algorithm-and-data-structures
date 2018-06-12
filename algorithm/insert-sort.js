3e8fd9f9c30d66bdced663eff102792955757c5d
Yin Jun <yinjunok@foxmail.com>
2018-06-12 15:58:09 +0800

    插入排序

---
+++ algorithm/insert-sort.js
@@ -0,0 +1,23 @@
+/**
+ * 插入排序. 跟摸牌很像.
+ * 一次只摸一张牌, 每次摸牌的时候, 都会把牌按大小插入到相应位置.
+*/
+
+const insertSort = (arr) => {
+  console.log(arr);
+  for (let i = 1; i < arr.length; ++i) {
+    for (let j = i - 1; j >= 0; --j) {
+      if (arr[j + 1] > arr[j]) {
+        break;
+      } else {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  console.log(arr);
+}
+
+let test = insertSort(randomGenerator(10));
