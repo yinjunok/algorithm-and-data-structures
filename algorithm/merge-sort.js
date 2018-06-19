feec2dbb3ff3089fcff4b49bead56f52ebd8f5fd
Yin Jun <yinjunok@foxmail.com>
2018-06-19 09:16:27 +0800

    归并排序

---
+++ algorithm/merge-sort.js
@@ -0,0 +1,28 @@
+const merge = (left, right) => {
+  const result = [];
+  // 对两个数组排序, 然后合并
+  while(left.length > 0 && right.length > 0) {
+    result.push(
+      left[0] > right[0] ? right.shift() : left.shift()
+    );
+  }
+  return [ ...result, ...left, ...right ];
+}
+
+const mergeSort = (arr) => {
+  const len = arr.length;
+  if (len < 2) {
+    return arr;
+  }
+  
+  const mid = Math.floor(len / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(
+    mergeSort(left), 
+    mergeSort(right),
+  );
+}
+
+console.log(mergeSort([1,23,23,12,41,41,24,124,2525,345,3,6,74,57,456]));
